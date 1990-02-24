1e37993922022b459c4bb361375f056c0b12f83d
Stan Zanarotti <srz@mit.edu>
1990-02-24 19:00:56 +0000

    Added support for signatures.  Also added "-d" (daemon) flag for deriving signatures for mail (daemon) transactions using the From: fields.

--- server/expunge.c
+++ server/expunge.c
@@ -16,6 +16,7 @@
 
 #include <stdio.h>
 #include <strings.h>
+#include <ctype.h>
 #include <sys/types.h>
 #include <sys/file.h>
 #include <sys/stat.h>
@@ -36,9 +37,13 @@ static char *future_location = NULL;
 static int found_eof = 0;
 static int error_occurred = 0;
 static char *temp_dir = "/tmp";
+static int daemon_flag = FALSE;
 
 tfile unix_tfile ();
 char *malloc();
+
+static lower(), strip_addr(), extract_full_name();
+static char *get_header();
 
 extern char rpc_caller[];
 extern int has_privs;
@@ -51,11 +56,12 @@ char **argv;
 {
      int i,n,low,high;
      mtg_info old_mtg_info,new_mtg_info;
-     trn_info old_trn_info;
+     trn_info3 old_trn_info;
      int result;
      dsc_acl *acl_list,*new_acl_list;
      dsc_acl_entry *ae;
-     char *new_modes;
+     char *new_modes, *signature;
+     char new_signature[50],dtest[10];
      tfile tf;
      char control_name[256];
      int control_fd;
@@ -77,6 +83,10 @@ char **argv;
 	   case 't':
 	       if (++i < argc)
 		   temp_dir = argv[i];
+	       continue;
+
+	  case 'd':
+	       daemon_flag = TRUE;
 	       continue;
 
 	  default:
@@ -149,7 +159,7 @@ char **argv;
 
 expunge_range:
      for (i = low; i <= high; i++) {
-	  get_trn_info (location, i, &old_trn_info, &result);
+	  get_trn_info3 (location, i, &old_trn_info, &result);
 	  if (result != 0 && result != DELETED_TRN && result != EXPUNGED_TRN) {
 	       fprintf(stderr,
 		       "Error getting info for transaction [%04d]: %s\n",
@@ -182,13 +192,23 @@ expunge_range:
 	       }
 
 	       tdestroy (tf);
-	       lseek(tempf,0,0);
 
+	       signature = old_trn_info.signature;
+	       if (daemon_flag && old_trn_info.signature != NULL) {
+		    bcopy(old_trn_info.signature, dtest, 7);
+		    dtest[7] = '\0';
+		    if (!strcmp(dtest,"daemon@")) {
+			 if (get_from_signature(tempf, new_signature, sizeof(new_signature)))
+			      signature = new_signature;
+		    }
+	       }
+	       lseek(tempf,0,0);
 	       tf = unix_tfile (tempf);
 	       no_nuke = TRUE;
 	       add_trn_priv (backup_location, tf, old_trn_info.subject,
-			     old_trn_info.pref, old_trn_info.current,
-			     old_trn_info.author, old_trn_info.date_entered,
+			     signature, old_trn_info.pref,
+			     old_trn_info.current, old_trn_info.author,
+			     old_trn_info.date_entered, old_trn_info.flags,
 			     &n, &result);
 	       no_nuke = FALSE;
 	       if (result != 0) {
@@ -199,6 +219,7 @@ expunge_range:
 	       }
 	       free(old_trn_info.author);
 	       free(old_trn_info.subject);
+	       free(old_trn_info.signature);
 	  }
      }
 
@@ -287,4 +308,289 @@ create_temp()
 	  fprintf (stderr, "Cannot open temp file\n");
 	  exit (1);
      }
+}
+
+/*
+ *
+ *   get_from_signature () -- Look in the file file_no, looking for a
+ *	From: line.  Construct a signature from this from line, returning
+ *	it in sign, with a maximum length of sign_len.
+ *
+ */
+
+int
+get_from_signature(file_no, sign, sign_len)
+int file_no;
+char *sign;
+int sign_len;
+{
+     char buf[2048];
+     int len;
+     char *cp, *hp;
+
+     lseek(file_no, 0, 0);
+     len = read(file_no, buf, sizeof(buf)-1);
+     buf[len] = '\0';
+
+     hp = get_header(buf, "from");
+     if (hp == NULL)
+	  return(0);
+
+     cp = index(hp, ':');
+     if (cp == NULL)
+	  return(0);
+
+     cp++;
+
+     extract_full_name(cp, sign, sign_len);
+     return(1);
+}
+
+/* Stolen from TechMail */
+
+static
+lower(s)
+char *s;
+{
+     while (*s) {
+	  if (isupper(*s))
+	       *s = tolower(*s);
+	  s++;
+     }
+     return;
+}
+
+static
+strip_addr(addr, dest, dest_size)
+char *addr,*dest;
+int dest_size;
+{
+     char *dest_end,*dp,*sp,*quote_start;
+     int paren_level,found_angle;
+
+     dest_end = &dest[dest_size-1];
+     dp = dest;
+     sp = addr;
+     paren_level = 0;
+
+eat_white:
+     while (isspace(*sp) && *sp != '\n')
+	  sp++;
+
+     if (*sp == '(')
+	  goto eat_comment;
+
+     if (*sp == '"')
+	  goto eat_string;
+
+     if (*sp == '<') {
+	  dp = dest;
+	  sp++;
+	  found_angle = TRUE;
+	  goto eat_white;
+     }
+
+     if (*sp == '>' && found_angle) {
+	  *sp++;
+	  goto eat_white;
+     }
+
+     if (*sp == '\0' || *sp == '\n') {
+	  *dp++ = '\0';
+	  goto post_proc;
+     }
+
+     *dp++ = *sp++;
+     if (dp == dest_end) {
+	  *dp++ = '\0';
+	  goto post_proc;
+     }
+
+     goto eat_white;
+
+eat_comment:
+     paren_level++;
+     sp++;
+
+cont_comment:
+     while (*sp != ')' && *sp != '(' && *sp) {
+	  sp++;
+     }
+
+     if (*sp == '\0') {
+	  *dp = '\0';
+	  goto post_proc;
+     }
+
+     if (*sp == '(')
+	  goto eat_comment;
+
+     sp++;		/* ) */
+     paren_level--;
+     if (paren_level <= 0)
+	  goto eat_white;
+
+     goto cont_comment;
+
+eat_string:
+     quote_start = sp;
+     sp++;
+
+     while(*sp != '"' && *sp) 
+	  sp++;
+
+     if (!*sp) {
+	  *dp = '\0';
+	  goto post_proc;
+     }
+
+     if (*++sp == '@') {		/* "foo"@bar */
+	  sp = quote_start;
+	  *dp++ = *sp++;
+	  while (dp < dest_end && *sp != '"')
+	       *dp++ = *sp++;
+
+	  if (dp == dest_end) {
+	       *dp = '\0';
+	       goto post_proc;
+	  }
+	  *dp++ = *sp++;
+	  if (dp == dest_end) {
+	       *dp++ = '\0';
+	       goto post_proc;
+	  }
+     }
+     goto eat_white;
+
+     /* No post processing */
+post_proc:
+     return;
+}
+
+/*
+ *
+ *   Routine to extract a full name from an address.  If no full name
+ *   can be found, then we simply return the stripped address.
+ *
+ */
+
+static
+extract_full_name(addr, dest, dest_size)
+char *addr,*dest;
+int dest_size;
+{
+     char *dest_end,*dp,*sp,*bracket,*close_paren;
+     int paren_level,non_white;
+
+     dest_end = &dest[dest_size-1];
+     dp = dest;
+     sp = addr;
+
+     /* Find angle bracket (if possible) */
+     while (*sp && *sp != '<' && *sp != '\n')
+	  sp++;
+
+     bracket = NULL;
+     if (*sp == '<')
+	  bracket = sp;
+
+     non_white = 0;
+     if (bracket != NULL) {
+	  for (sp = addr; sp < bracket; sp++) {
+	       if (!isspace(*sp) && *sp != '"')
+		    non_white++;
+	  }
+     }
+
+     if (non_white > 1) {		/* We have a name */
+	  sp = addr;
+	  while (isspace(*sp) || *sp == '"')	/* Skip leading spaces */
+	       sp++;
+
+	  while (isspace(*(bracket-1)) || *(bracket-1) == '"')  /* Skip trailing spaces */
+	       bracket--;
+
+	  /* Copy it over */
+	  while (sp < bracket && dp < dest_end)
+	       *dp++ = *sp++;
+
+	  *dp++ = '\0';
+	  return;
+     }
+
+     /* Now, let's see if we have name in a comment (look back from the
+	end for a parenthesis. */
+     for (sp = addr; *sp && *sp != '\n'; sp++)
+	  ;
+
+     sp--;
+     while (sp > addr && isspace(*sp))
+	  sp--;
+
+     if (*sp == ')') {			/* Name in comment */
+	  close_paren = sp;
+	  paren_level = 1;
+	  sp--;
+
+	  for (;sp > addr; sp--) {
+	       if (*sp == ')')
+		    paren_level++;
+	       else if (*sp == '(') {
+		    paren_level--;
+		    if (paren_level == 0)
+			 break;
+	       }
+	  }
+
+	  if (*sp == '(') {	      	/* Copy it over */
+	       sp++;
+
+	       while(isspace(*sp))
+		    sp++;
+
+	       while (sp < close_paren && dp < dest_end)
+		    *dp++ = *sp++;
+
+	       *dp = '\0';
+	       return;
+	  }
+     }
+
+     strip_addr(addr, dest, dest_size);
+     return;
+}	  
+
+static char *
+get_header(hstring, hname)
+char *hstring, *hname;
+{
+     char *eolp, *colonp, *cp;
+     int name_len;
+     char field[33];
+
+     cp = hstring;
+     name_len = strlen(hname);
+
+     while (*cp != '\0') {
+	  eolp = index(cp, '\n');
+	  colonp = index(cp, ':');
+
+	  if (eolp == NULL || colonp == NULL || eolp == cp)
+	       return(0);
+
+	  if (colonp > eolp || colonp - cp != name_len) {
+	       cp = eolp+1;
+	       continue;
+	  }
+
+	  /* Chance of a match, copy header over to name, and lower it */
+	  bcopy(cp, field, colonp - cp);
+	  field[colonp - cp] = '\0';
+	  lower(field);
+	  if (!strcmp(field,hname))
+	       return(cp);
+	  cp = eolp + 1;
+     }
+
+     return(NULL);
 }
